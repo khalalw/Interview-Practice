187b264a7a50e4fce68bc4f5b3e8fa2f78dcde7c
Khalal <khalal.walker@gmail.com>
2019-10-14 23:41:03 -0700

    reverse string in place

---
+++ interview-cake/reverse_string_in_place/reverseString.js
@@ -0,0 +1,41 @@
+const reverse = arrayOfChars => {
+  const swap = (arr, a, b) => {
+    const temp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = temp;
+  };
+  for (let i = 0; i < arrayOfChars.length / 2; i += 1) {
+    swap(arrayOfChars, i, arrayOfChars.length - i - 1);
+  }
+};
+
+//  Tests
+
+const assertEqual = (a, b, desc) => {
+  if (a === b) {
+    console.log(`${desc} ... PASS`);
+  } else {
+    console.log(`${desc} ... FAIL: ${a} != ${b}`);
+  }
+};
+
+let desc = 'empty string';
+let input = ''.split('');
+reverse(input);
+let actual = input.join('');
+let expected = '';
+assertEqual(actual, expected, desc);
+
+desc = 'single character string';
+input = 'A'.split('');
+reverse(input);
+actual = input.join('');
+expected = 'A';
+assertEqual(actual, expected, desc);
+
+desc = 'longer string';
+input = 'ABCDE'.split('');
+reverse(input);
+actual = input.join('');
+expected = 'EDCBA';
+assertEqual(actual, expected, desc);
