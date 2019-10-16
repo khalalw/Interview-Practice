99969eb69476d048011e36b00dc8b05603b48f5d
Khalal <khalal.walker@gmail.com>
2019-10-15 17:53:39 -0700

    merge sorted arrays solution

---
+++ interview-cake/merge_sorted_arrays/mergeArrays.js
@@ -0,0 +1,51 @@
+/* eslint-disable no-console */
+const mergeArrays = (arr1, arr2) => {
+  const res = [];
+
+  while (arr1.length && arr2.length) {
+    if (arr1[0] > arr2[0]) {
+      res.push(arr2.shift());
+    } else {
+      res.push(arr1.shift());
+    }
+  }
+
+  return res.concat(arr1.length ? arr1 : arr2);
+};
+
+// Tests
+
+const assertDeepEqual = (a, b, desc) => {
+  const aStr = JSON.stringify(a);
+  const bStr = JSON.stringify(b);
+  if (aStr !== bStr) {
+    console.log(`${desc} ... FAIL: ${aStr} != ${bStr}`);
+  } else {
+    console.log(`${desc} ... PASS`);
+  }
+};
+
+let desc = 'both arrays are empty';
+let actual = mergeArrays([], []);
+let expected = [];
+assertDeepEqual(actual, expected, desc);
+
+desc = 'first array is empty';
+actual = mergeArrays([], [1, 2, 3]);
+expected = [1, 2, 3];
+assertDeepEqual(actual, expected, desc);
+
+desc = 'second array is empty';
+actual = mergeArrays([5, 6, 7], []);
+expected = [5, 6, 7];
+assertDeepEqual(actual, expected, desc);
+
+desc = 'both arrays have some numbers';
+actual = mergeArrays([2, 4, 6], [1, 3, 7]);
+expected = [1, 2, 3, 4, 6, 7];
+assertDeepEqual(actual, expected, desc);
+
+desc = 'arrays are different lengths';
+actual = mergeArrays([2, 4, 6, 8], [1, 7]);
+expected = [1, 2, 4, 6, 7, 8];
+assertDeepEqual(actual, expected, desc);
